fa38ca6c393e2ce49cb476ec4e8c11dce70a9681
Thibault Jaillard <thibault.jaillard@gmail.com>
2025-07-07 21:48:11 -0400

    fix: seo errors fixed

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,5 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 export default [
   index('routes/terminal/terminal.tsx'),
   route('portfolio', 'routes/portfolio/portfolio.tsx'),
-  route('sitemap.xml', 'routes/sitemap.tsx'),
   route('.well-known/*', 'routes/well-known.tsx'),
 ] satisfies RouteConfig;
